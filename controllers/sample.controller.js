cd0a595013cd9b4c0992f13ec4f939590d55eb26
kershadi <kouros.ershadi@epoolex.com>
2020-04-22 22:58:51 -0400

    update: make id as string within double quote

--- controllers/sample.controller.js
+++ controllers/sample.controller.js
@@ -2,7 +2,7 @@ var express = require ('express');
 var router = express.Router();
 
 router.get('/sample', (req,res)=>{
-    res.send('{id: "Node.js sample API works"}');
+    res.send('{"id": "Node.js sample API works"}');
 })
 
 module.exports = router;
